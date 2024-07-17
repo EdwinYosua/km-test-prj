004530e1830e8327cd753aef5aa15dcf47be1328
Edwin Yosua <115564225+EdwinYosua@users.noreply.github.com>
2024-07-17 15:21:18 +0800

    SecondFragment
    adding inset to fix enableEdgeToEdge overlap in SecondFragment

--- app/src/main/java/com/edwinyosua/kmtest/MainActivity.kt
+++ app/src/main/java/com/edwinyosua/kmtest/MainActivity.kt
@@ -5,13 +5,19 @@ import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.app.AppCompatDelegate
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import com.edwinyosua.kmtest.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         installSplashScreen()
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
         AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO) //disable night mode in app
+
     }
 }
--- app/src/main/java/com/edwinyosua/kmtest/ui/fragment/second/SecondFragment.kt
+++ app/src/main/java/com/edwinyosua/kmtest/ui/fragment/second/SecondFragment.kt
@@ -4,6 +4,9 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updateLayoutParams
 import androidx.fragment.app.Fragment
 import androidx.navigation.fragment.findNavController
 import com.edwinyosua.kmtest.databinding.FragmentSecondBinding
@@ -23,15 +26,26 @@ class SecondFragment : Fragment() {
         // Inflate the layout for this fragment
 //        return inflater.inflate(R.layout.fragment_second, container, false)
 
-
         _binding = FragmentSecondBinding.inflate(layoutInflater, container, false)
         return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+
+        //inset for enableEdgeToEdge()
+        ViewCompat.setOnApplyWindowInsetsListener(view) { v, windowsInsets ->
+            val insets = windowsInsets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.updateLayoutParams<ViewGroup.MarginLayoutParams> {
+                topMargin = insets.top
+                bottomMargin = insets.bottom
+            }
+            WindowInsetsCompat.CONSUMED
+        }
+
         binding.apply {
-            txtName.text = com.edwinyosua.kmtest.ui.fragment.SecondFragmentArgs.fromBundle(arguments as Bundle).name
+            txtName.text = SecondFragmentArgs.fromBundle(arguments as Bundle).name
 
             btnBack.setOnClickListener {
                 findNavController().popBackStack()
